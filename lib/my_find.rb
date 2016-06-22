82fec8829e526206153c423dfd09b5bff6adb701
FrantzLacarriere <frantz.lacarriere@gmail.com>
2016-06-22 14:43:22 -0400

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  ret = nil
+  while i < collection.length
+    if (yield(collection[i]))
+      ret = collection[i]
+      break
+    end
+    i += 1
+  end
+  ret
 end
